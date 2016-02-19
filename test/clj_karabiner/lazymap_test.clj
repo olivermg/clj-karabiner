153bb48342925b46111dbd7f065d6ee1133b5ca7
Oliver Wegner <void1976@gmail.com>
2016-02-19 17:02:37 +0100

    extended tests for lazymap

--- test/clj_karabiner/lazymap_test.clj
+++ test/clj_karabiner/lazymap_test.clj
@@ -4,7 +4,7 @@
             [clojure.data :as d]))
 
 (deftest basic-1
-  (testing "testing basic properties of this custom datatype"
+  (testing "basic properties of this custom datatype"
     (let [m1 (new-lazymap {:a [1 2 3] :b 22 :c 33})
           m11 (new-lazymap {:a [1 2 3] :b 22 :c 33})
           m2 (assoc m1 :c 333)
@@ -16,11 +16,20 @@
       (is (coll? m1))
       (is (map? m1))
       (is (instance? clojure.lang.IPersistentMap m1))
+      (is (instance? clojure.lang.IPersistentCollection m1))
       (is (instance? java.util.Map m1))
       (is (= m1 m1))
       (is (= m1 m11))
       (is (= m1 {:a [1 2 3] :b 22 :c 33}))
       (is (= m2 m2))
-      (is (= (type m1) (type m5)))
+      (is (= (type m1) (type m2) (type m3) (type m4) (type m5)
+             clj_karabiner.lazymap.LazyMap))
       (is (= (d/diff (.-contents m1) (.-contents m5)) '({:b 22 :c 33} {:c 3333 :d 44} {:a [1 2 3]})))
       (is (= (d/diff m1 m5) '({:b 22 :c 33} {:c 3333 :d 44} {:a [1 2 3]}))))))
+
+(deftest laziness-1
+  (testing "lazyness"
+    (let [m1 (new-lazymap {:a 11 :b (delay 22)})]
+      (is (= (m1 :a) 11))
+      (is (= (m1 :b) 22))
+      (is (= (:b m1) 22)))))

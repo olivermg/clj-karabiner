7999e2b816898d8cf3bdcb6f6afa681ff52ef09f
Oliver Wegner <void1976@gmail.com>
2018-06-19 19:06:48 +0200

    more sample invocations

--- src/clj_karabiner/tree/serialization.clj
+++ src/clj_karabiner/tree/serialization.clj
@@ -75,17 +75,18 @@
 ;;; sample invocations
 ;;;
 
-#_(let [ms   (clj-karabiner.external-memory.atom/atom-storage {:readers bp/edn-readers})
-                                        mem  (em/external-memory ms)
-                                        ln1  (bt/->B+TreeLeafNode 3 1 {[:a 1] 11})
-                                        ln2  (bt/->B+TreeLeafNode 3 2 {[:a 2] 22
-                                                                      [:a 3] 33})
-                                        in1  (bt/->B+TreeInternalNode 3 2 [[:a 1]] [ln1 ln2])
-                                        ln1p (em/save mem ln1)
-                                        ln2p (em/save mem ln2)
-                                        in1p (em/save mem in1)
-                                        ln1r (em/load mem ln1p)
-                                        ln2r (em/load mem ln2p)
-                                        in1r (em/load mem in1p)]
-                                    [(= ln1 ln1r) (= ln2 ln2r) (= in1 in1r)]
-                                    [in1 in1r])
+#_(let [ms (clj-karabiner.external-memory.atom/atom-storage {:readers bp/edn-readers})
+      mem  (em/external-memory ms)
+      ln1  (bt/->B+TreeLeafNode 3 1 {[:a 1] 11})
+      ln2  (bt/->B+TreeLeafNode 3 2 {[:a 2] 22
+                                     [:a 3] 33})
+      in1  (bt/->B+TreeInternalNode 3 2 [[:a 1]] [ln1 ln2])
+      ln1p (em/save mem ln1)
+      ln2p (em/save mem ln2)
+      in1p (em/save mem in1)
+      ln1r (em/load mem ln1p)
+      ln2r (em/load mem ln2p)
+      in1r (em/load mem in1p)]
+  [(= ln1 ln1r) (= ln2 ln2r) (= in1 in1r)]
+  (let [[_ n] (t/lookup in1r [:a 1])]
+    (em/load mem n)))

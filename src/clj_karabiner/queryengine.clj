bca8fdb3680285fdadea2a6d50de8bed4551eb4c
Oliver Wegner <void1976@gmail.com>
2018-08-01 21:37:50 +0200

    adding queryengine & storageengine
    high level data structures to be used by users

---
+++ src/clj_karabiner/queryengine.clj
@@ -0,0 +1,7 @@
+(ns clj-karabiner.queryengine)
+
+
+;;; engine for doing querying in the sense of CQRS
+
+
+(defrecord QueryEngine [])
---
+++ src/clj_karabiner/storageengine.clj
@@ -0,0 +1,7 @@
+(ns clj-karabiner.storageengine)
+
+
+;;; engine for storing (=appending) data in the sense of CQRS
+
+
+(defrecord StorageEngine [])

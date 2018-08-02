64b4c7617c26b0488c31b3a724b5fead2600be34
Oliver Wegner <void1976@gmail.com>
2018-08-02 15:21:58 +0200

    implementing queryengine & fact-database stuff

--- src/clj_karabiner/fact_database.clj
+++ src/clj_karabiner/fact_database.clj
@@ -1,15 +1,18 @@
-(ns clj-karabiner.database
+(ns clj-karabiner.fact-database
   (:require [clj-karabiner.tree :as t]
             [clj-karabiner.tree.bplustree :as bp]
             [clj-karabiner.external-memory :as em]
-            [clj-karabiner.external-memory.atom :as ema]))
+            [clj-karabiner.external-memory.atom :as ema]
+            [clj-karabiner.queryengine :as qe]
+            [clj-karabiner.fact-database-value :as fdv]))
 
 
-(defrecord FactDatabase [generation-count eavt-indices aevt-indices])
-(defrecord FactDatabaseValue [eavt-index aevt-index])
+;;; NOTE: we don't need an avet index, as we can use the vaet index for [a v ...] lookups:
+(defrecord FactDatabase [generation-count eavts aevts vaets eas])
 
 
-(defn append [{:keys [generation-count eavt-indices aevt-indices] :as this} [e a v t :as fact]]
+(defn append [{:keys [generation-count eavts aevts vaets eas] :as this}
+              [e a v t :as fact]]
 
   (letfn [(evolve-indices [[index0 & rest-indices :as indices] k fact]
             (take generation-count
@@ -17,33 +20,38 @@
                       (cons indices))))]
 
     (->FactDatabase generation-count
-                    (evolve-indices eavt-indices [e a v t] fact)
-                    (evolve-indices aevt-indices [a e v t] fact))))
+                    (evolve-indices eavts fact      fact)
+                    (evolve-indices aevts [a e v t] fact)
+                    (evolve-indices vaets [v a e t] fact)
+                    (evolve-indices eas   [e a]     fact)  ;; to keep track of most current t that touched this [e a]
+                    )))
 
 
 (defn get-database-value
-  ([{:keys [eavt-indices aevt-indices] :as this} generation]
-   (->FactDatabaseValue (nth eavt-indices generation)
-                        (nth aevt-indices generation)))
+  ;;; TODO: generation should be t/tx
+  ([{:keys [eavts aevts vaets eas] :as this} generation]
+   (-> (fdv/->FactDatabaseValue (nth eavts generation)
+                                (nth aevts generation)
+                                (nth vaets generation)
+                                (nth eas   generation))
+       (qe/queryengine)))
 
   ([this]
    (get-database-value this 0)))
-
-
-(defn lookup-eavt [{:keys [eavt-index] :as this} k]
-  (t/lookup-range eavt-index k))
-
-
-(defn lookup-aevt [{:keys [aevt-index] :as this} k]
-  (t/lookup-range aevt-index k))
 
 
 (defn database [& {:keys [b+tree-branching-factor
                           generation-count]
                    :or {b+tree-branching-factor 1000
                         generation-count 1000}}]
-  (let [eavt-index (bp/b+tree b+tree-branching-factor
-                              (em/external-memory (ema/atom-storage)))
-        aevt-index (bp/b+tree b+tree-branching-factor
-                              (em/external-memory (ema/atom-storage)))]
-    (->FactDatabase generation-count (list eavt-index) (list aevt-index))))
+
+  (letfn [(make-index []
+            (bp/b+tree b+tree-branching-factor
+                       (em/external-memory (ema/atom-storage))))]
+
+    (->FactDatabase generation-count
+                    (list (make-index))  ;; eavt
+                    (list (make-index))  ;; aevt
+                    (list (make-index))  ;; vaet
+                    (list (make-index))  ;; ea
+                    )))

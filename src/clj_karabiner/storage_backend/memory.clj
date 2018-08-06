cf21545c1a8592f5fd78cdacf33ce15949435b4d
Oliver Wegner <void1976@gmail.com>
2018-08-06 11:57:44 +0200

    implementing simple in-memory storage-backend

---
+++ src/clj_karabiner/storage_backend/memory.clj
@@ -0,0 +1,19 @@
+(ns clj-karabiner.storage-backend.memory
+  (:require [clj-karabiner.storage-backend :as sb]))
+
+
+(defrecord MemoryStorageBackend [storage]
+
+  sb/LoadableStorageBackend
+
+  (load [this]
+    (lazy-seq @storage))
+
+  sb/AppendableStorageBackend
+
+  (append [this obj]
+    (swap! storage #(conj % obj))))
+
+
+(defn memory-storage-backend []
+  (map->MemoryStorageBackend {:storage (atom [])}))

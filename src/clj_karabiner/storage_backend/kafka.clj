7d1435000a29ce680d3f20457a91d8c353662f71
Oliver Wegner <void1976@gmail.com>
2018-08-17 10:43:10 +0200

    implementing transit+json de/serializer for kafka
    adding topic-prefix functionality

--- src/clj_karabiner/storage_backend/kafka.clj
+++ src/clj_karabiner/storage_backend/kafka.clj
@@ -1,9 +1,42 @@
 (ns clj-karabiner.storage-backend.kafka
-  (:require #_[clojure.core.async :refer [go go-loop <! >! put!] :as a]
+  (:require [clojure.string :as str]
+            [cognitect.transit :as t]
+            #_[clojure.core.async :refer [go go-loop <! >! put!] :as a]
             [clj-karabiner.storage-backend :as sb])
-  (:import [org.apache.kafka.clients.admin AdminClient NewTopic]
+  (:import [java.io ByteArrayOutputStream ByteArrayInputStream]
+           [org.apache.kafka.clients.admin AdminClient NewTopic]
            [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]
-           [org.apache.kafka.clients.consumer KafkaConsumer]))
+           [org.apache.kafka.clients.consumer KafkaConsumer]
+           [org.apache.kafka.common.serialization Serializer Deserializer]))
+
+
+(defrecord TransitSerializer []
+
+  Serializer
+
+  (close [this])
+
+  (configure [this configs isKey])
+
+  (serialize [this topic data]
+    (let [out (ByteArrayOutputStream. 10)
+          w (t/writer out :json)]
+      (t/write w data)
+      (.toByteArray out))))
+
+
+(defrecord TransitDeserializer []
+
+  Deserializer
+
+  (close [this])
+
+  (configure [this configs isKey])
+
+  (deserialize [this topic data]
+    (let [in (ByteArrayInputStream. data)
+          r (t/reader in :json)]
+      (t/read r))))
 
 
 (defn- topic-exists? [{:keys [topics] :as this} topic]
@@ -17,23 +50,25 @@
   (swap! topics #(conj % topic)))
 
 
-(defn- list-topics [{:keys [admin] :as this}]
+(defn- list-topics [{:keys [topic-prefix admin] :as this}]
   (let [topics-result (.listTopics admin)
         topics (.get (.listings topics-result))]
-    (->> (remove #(= (.isInternal %) true) topics)
+    (->> topics
+         (remove #(or (= (.isInternal %) true)
+                      (not (str/starts-with? (.name %) topic-prefix))))
          (map #(.name %))
          set)))
 
 
-(defrecord KafkaStorageBackend [bootstrap-server topic-fn key-fn value-fn partition-count replication-factor
+(defrecord KafkaStorageBackend [topic-prefix bootstrap-server topic-fn key-fn value-fn partition-count replication-factor
                                 topics admin producer]
 
   sb/LoadableStorageBackend
 
   (load [this]
     (let [consumer (KafkaConsumer. {"bootstrap.servers"  bootstrap-server
-                                    "key.deserializer"   "org.apache.kafka.common.serialization.StringDeserializer"
-                                    "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+                                    "key.deserializer"   "clj_karabiner.storage_backend.kafka.TransitDeserializer"
+                                    "value.deserializer" "clj_karabiner.storage_backend.kafka.TransitDeserializer"
                                     "group.id"           (str (java.util.UUID/randomUUID))
                                     "max.poll.records"   "500"
                                     "enable.auto.commit" "false"})]
@@ -43,12 +78,15 @@
         (.seekToBeginning (.assignment consumer)))
       (letfn [(get-chunks []
                 (lazy-seq
+                 (println "I/O")
                  (let [crs (.poll consumer 250)]
                    (if (and crs (> (.count crs) 0))
                      (concat (map (fn [cr]
                                     {:key (.key cr)
                                      :value (.value cr)
-                                     :topic (.topic cr)
+                                     :topic (->> (.topic cr)
+                                                 (drop (count topic-prefix))
+                                                 (apply str))
                                      :partition (.partition cr)
                                      :offset (.offset cr)})
                                   crs)
@@ -60,7 +98,7 @@
   sb/AppendableStorageBackend
 
   (append [this obj]
-    (let [topic (topic-fn obj)]
+    (let [topic (str topic-prefix (topic-fn obj))]
       (when-not (topic-exists? this topic)
         (create-topic this topic))
       (let [key (key-fn obj)
@@ -73,13 +111,14 @@
          :timestamp (.timestamp record-meta)}))))
 
 
-(defn kafka-storage-backend [& {:keys [bootstrap-server topic-fn key-fn value-fn partition-count replication-factor]
+(defn kafka-storage-backend [& {:keys [topic-prefix bootstrap-server topic-fn key-fn value-fn partition-count replication-factor]
                                 :or {bootstrap-server "localhost:9092"}}]
   (let [admin (AdminClient/create {"bootstrap.servers" bootstrap-server})
         producer (KafkaProducer. {"bootstrap.servers" bootstrap-server
-                                  "key.serializer"    "org.apache.kafka.common.serialization.StringSerializer"
-                                  "value.serializer"  "org.apache.kafka.common.serialization.StringSerializer"})
-        ksb (map->KafkaStorageBackend {:bootstrap-server   bootstrap-server
+                                  "key.serializer"    "clj_karabiner.storage_backend.kafka.TransitSerializer"
+                                  "value.serializer"  "clj_karabiner.storage_backend.kafka.TransitSerializer"})
+        ksb (map->KafkaStorageBackend {:topic-prefix       (str (or topic-prefix "storage") "-")
+                                       :bootstrap-server   bootstrap-server
                                        :topic-fn           (or topic-fn :topic)
                                        :key-fn             (or key-fn :key)
                                        :value-fn           (or value-fn :value)

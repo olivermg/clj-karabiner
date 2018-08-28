783b258e555d17944cdd7181c050f7aed6b30f96
Oliver Wegner <void1976@gmail.com>
2018-08-28 15:54:44 +0200

    separating dedup-list from sized-cache

--- src/clj_karabiner/cache/sized_cache.clj
+++ src/clj_karabiner/cache/sized_cache.clj
@@ -1,53 +1,6 @@
 (ns clj-karabiner.cache.sized-cache
-  (:require [clj-karabiner.cache :as c]))
-
-
-(deftype DedupList [l s maxlen curlen]
-
-  clojure.lang.IPersistentCollection
-
-  (count [this]
-    (.count l))
-
-  (empty [this]
-    (DedupList. '() #{} nil 0))
-
-  (equiv [this o]
-    (.equiv l (:l o)))
-
-  (cons [this o]
-    (if (contains? s o)
-      (DedupList. (.cons (remove #(= % o) l) o) s maxlen curlen)
-      (let [ncurlen (inc curlen)]
-        (if-not maxlen
-          (DedupList. (.cons l o) (conj s o) maxlen ncurlen)
-          (if (<= ncurlen maxlen)
-            (DedupList. (.cons l o) (conj s o) maxlen ncurlen)
-            (DedupList. (take maxlen (.cons l o))
-                        (-> (conj s o)
-                            (disj (last l)))
-                        maxlen
-                        curlen))))))
-
-  clojure.lang.Seqable
-
-  (seq [this]
-    (.seq l))
-
-  clojure.lang.ISeq
-
-  (first [this]
-    (.first l))
-
-  (next [this]
-    (.next l))
-
-  (more [this]
-    (.more l)))
-
-
-(defn dedup-list [& {:keys [maxlen]}]
-  (->DedupList '() #{} maxlen 0))
+  (:require [clj-karabiner.cache :as c]
+            [clj-karabiner.dedup-list :as ddl]))
 
 
 (defrecord SizedCache [l m]
@@ -72,4 +25,4 @@
 
 
 (defn sized-cache [size]
-  (->SizedCache (dedup-list :maxlen size) {}))
+  (->SizedCache (ddl/dedup-list :maxlen size) {}))

955a5ea10bfc8cfcb37670eb73519f3ae8a58bb1
Oliver Wegner <void1976@gmail.com>
2018-08-28 16:46:17 +0200

    swappable now knows swapper which in turn handles loading/saving nodes from/to cache & storage

--- src/clj_karabiner/tree/swappable.clj
+++ src/clj_karabiner/tree/swappable.clj
@@ -1,35 +1,58 @@
 (ns clj-karabiner.tree.swappable
-  (:require [clj-karabiner.tree :as t]))
+  (:require [clj-karabiner.tree :as t]
+            [clj-karabiner.cache :as c]
+            [clj-karabiner.external-memory :as em]))
+
+
+(defrecord NodeSwapper [cache storage])
+
+
+(defn swap-in [{:keys [cache storage] :as this} id]
+  (if-let [node (c/lookup cache id)]
+    node
+    (if-let [node (em/load storage id)]
+      (do (c/store cache id node)
+          node)
+      (throw (ex-info "unknown node with id" {:id id})))))
+
+
+(defn swap-out [{:keys [cache storage] :as this} id node]
+  #_(c/store cache id node)
+  (em/save storage node)
+  nil)
 
 
 (declare swappable-node)
 
 
-(defrecord SwappableNode [node-id node]
+(defrecord SwappableNode [swapper id]
 
   t/Node
 
   (id [this]
-    node-id)
+    (:id this))
 
   t/ModifyableNode
 
   (insert* [this k v user-data]
-    (let [[n1 nk n2 lnbs lv] (t/insert* node k v user-data)
-          sn1 (swappable-node n1)
-          sn2 (and n2 (swappable-node n2))]
+    (let [node (swap-in swapper id)
+          [n1 nk n2 lnbs lv] (t/insert* node k v user-data)
+          sn1 (swappable-node swapper n1)
+          sn2 (and n2 (swappable-node swapper n2))]
       #_(println "SWAPPABLE INSERT" (:id n1) (:id n2))
       [sn1 nk sn2 lnbs lv]))
 
   t/LookupableNode
 
   (lookup* [this k user-data]
-    (t/lookup* node k user-data))
+    (t/lookup* (swap-in swapper id) k user-data))
 
   (lookup-range* [this k user-data]
-    (t/lookup-range* node k user-data)))
+    (t/lookup-range* (swap-in swapper id) k user-data)))
 
 
-(defn swappable-node [node]
-  (map->SwappableNode {:node-id (t/id node)
-                       :node node}))
+(defn swappable-node [swapper node]
+  (let [id (t/id node)]
+    (swap-out swapper id node)
+    (map->SwappableNode {:swapper swapper
+                         :id      id})))

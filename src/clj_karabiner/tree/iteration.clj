f54032733d22f3dc2a80c9321e7f1f7447e20e8c
Oliver Wegner <void1976@gmail.com>
2018-08-31 09:03:05 +0200

    don't need iteration at this moment

--- src/clj_karabiner/tree/iteration.clj
+++ src/clj_karabiner/tree/iteration.clj
@@ -1,14 +1,14 @@
 (ns clj-karabiner.tree.iteration
-  (:require [clj-karabiner.cache :as c]))
+  (:require [clj-karabiner.kvstore :as kvs]))
 
 
 (defrecord IterationResult [leaf-neighbours last-visited])
 
 
-(defn combine [this other]
+#_(defn combine [this other]
   (->IterationResult (merge (:leaf-neighbours this)
                             (:leaf-neighbours other))
                      (reduce (fn [s e]
-                               (c/store s e (c/lookup (:last-visited other) e)))
+                               (kvs/store s e (c/lookup (:last-visited other) e)))
                              (:last-visited this)
-                             (c/keys (:last-visited other)))))
+                             (kvs/keys (:last-visited other)))))

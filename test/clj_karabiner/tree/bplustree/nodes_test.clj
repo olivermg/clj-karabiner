6f01769d702ef2f9574cdbd7724bfa21199d98c7
Oliver Wegner <void1976@gmail.com>
2018-08-31 11:45:03 +0200

    adjusting tests for b+tree nodes

--- test/clj_karabiner/tree/bplustree/nodes_test.clj
+++ test/clj_karabiner/tree/bplustree/nodes_test.clj
@@ -3,12 +3,12 @@
             [clj-karabiner.tree.bplustree.nodes :refer :all]
             [clj-karabiner.tree.bplustree :as bpt]
             [clj-karabiner.keycomparator.partial-keycomparator :as pkc]
-            #_[clj-karabiner.external-memory.atom :as aem]
+            #_[clj-karabiner.kvstore.atom :as kva]
             #_[clj-karabiner.cache :as c]
             [clojure.pprint]))
 
 
-#_(deftest internal-node-lookup-logic-1
+(deftest internal-node-lookup-logic-1
   (letfn [(build-this []
             (let [ks [     2         6       9             13       15          18       20       22       24          27          30]
                   vs [[1 2] [3 4 5 6] [7 8 9] [10 11 12 13]  [14 15]  [16 17 18]  [19 20]  [21 22]  [23 24]  [25 26 27]  [28 29 30]  [31 32 33]]]
@@ -33,56 +33,52 @@
                :size (count ks)}))
 
           (build-userdata []
-            (let [key-comparator (pkc/partial-key-comparator)
-                  external-memory (aem/atom-external-memory)
-                  last-visited (c/sized-cache 3)]
+            (let [key-comparator (pkc/partial-key-comparator)]
               {:key-comparator key-comparator
-               :external-memory external-memory
-               :leaf-neighbours {}
-               :last-visited last-visited}))]
+               :leaf-neighbours {}}))]
 
     (let [this (build-this)
           user-data (build-userdata)]
       (testing "min lookup"
-        (let [[k v] (internal-lookup* this 1 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 1 user-data)]
           (is (= 2 k))
           (is (= [1 2] v))))
       (testing "lookup 1"
-        (let [[k v] (internal-lookup* this 7 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 7 user-data)]
           (is (= 9 k))
           (is (= [7 8 9] v))))
       (testing "lookup 2"
-        (let [[k v] (internal-lookup* this 4 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 4 user-data)]
           (is (= 6 k))
           (is (= [3 4 5 6] v))))
       (testing "lookup 3"
-        (let [[k v] (internal-lookup* this 23 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 23 user-data)]
           (is (= 24 k))
           (is (= [23 24] v))))
       (testing "lookup 4"
-        (let [[k v] (internal-lookup* this 29 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 29 user-data)]
           (is (= 30 k))
           (is (= [28 29 30] v))))
       (testing "max lookup"
-        (let [[k v] (internal-lookup* this 321 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 321 user-data)]
           (is (= :clj-karabiner.tree.bplustree.nodes/inf k))
           (is (= [31 32 33] v)))))
 
     (let [this (build-this-2)
           user-data (build-userdata)]
       (testing "max lookup"
-        (let [[k v] (internal-lookup* this [:a 2] user-data)]
+        (let [{k :actual-k v :value} (lookup-local this [:a 2] user-data)]
           (is (= [:a 4] k))
           (is (= {:b 3 :size 2 :m {[:a 3] 33 [:a 4] 44}} v)))))
 
     (let [this (build-this-3)
           user-data (build-userdata)]
       (testing "lookup 1"
-        (let [[k v] (internal-lookup* this 1 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 1 user-data)]
           (is (= 3 k))
           (is (= [1 2 3] v))))
       (testing "lookup 2"
-        (let [[k v] (internal-lookup* this 5 user-data)]
+        (let [{k :actual-k v :value} (lookup-local this 5 user-data)]
           (is (= :clj-karabiner.tree.bplustree.nodes/inf k))
           (is (= [4 5 6] v)))))))
 

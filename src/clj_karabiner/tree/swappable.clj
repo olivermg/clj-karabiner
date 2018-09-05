88e53a72910c0b1ee1896d3e528b5f9b16c01e26
Oliver Wegner <void1976@gmail.com>
2018-09-05 11:37:54 +0200

    swappable node doesn't need to store kvstore anymore

--- src/clj_karabiner/tree/swappable.clj
+++ src/clj_karabiner/tree/swappable.clj
@@ -6,7 +6,7 @@
 (declare swappable-node)
 
 
-(defrecord SwappableNode [kvstore id]
+(defrecord SwappableNode [id]
 
   t/Node
 
@@ -15,24 +15,23 @@
 
   t/ModifyableNode
 
-  (insert* [this k v t]
-    (let [node (kvs/lookup kvstore id)
-          [n1 nk n2 lnbs lv] (t/insert* node k v t)
-          sn1 (swappable-node kvstore n1)
-          sn2 (and n2 (swappable-node kvstore n2))]
+  (insert* [this k v {:keys [node-kvstore] :as t}]
+    (let [node (kvs/lookup node-kvstore id)
+          [n1 nk n2 lnbs lv] (t/insert node k v :tree t)
+          sn1 (swappable-node node-kvstore n1)
+          sn2 (and n2 (swappable-node node-kvstore n2))]
       [sn1 nk sn2 lnbs lv]))
 
   t/LookupableNode
 
-  (lookup* [this k t]
-    (t/lookup* (kvs/lookup kvstore id) k t))
+  (lookup* [this k {:keys [node-kvstore] :as t}]
+    (t/lookup (kvs/lookup node-kvstore id) k :tree t))
 
-  (lookup-range* [this k t]
-    (t/lookup-range* (kvs/lookup kvstore id) k t)))
+  (lookup-range* [this k {:keys [node-kvstore] :as t}]
+    (t/lookup-range (kvs/lookup node-kvstore id) k :tree t)))
 
 
-(defn swappable-node [kvstore node]
+(defn swappable-node [node-kvstore node]
   (let [id (t/id node)]
-    (kvs/store kvstore id node)
-    (map->SwappableNode {:kvstore kvstore
-                         :id      id})))
+    (kvs/store node-kvstore id node)
+    (map->SwappableNode {:id id})))
